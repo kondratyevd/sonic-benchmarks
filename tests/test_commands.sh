d3a056e04ebf87abdac46882850ba3c82959d31d
Dmitry <kondratyev.d.95@gmail.com>
2024-02-02 00:41:33 +0100

    update commands

--- tests/test_commands.sh
+++ tests/test_commands.sh
@@ -3,11 +3,13 @@ git config --global user.name "Dmitry Kondratyev"
 git config --global user.github "kondratyevd"
 
 # sudo yum install -y openssl-devel
+ls -l
+pwd
 
-sudo chmod +x setup.sh
-sudo mkdir cmssw
-sudo chmod -R 777 cmssw
-cd cmssw
+chmod +x setup.sh
+# mkdir cmssw
+# chmod -R 777 cmssw
+# cd cmssw
 ../setup.sh -a https
 
 cd CMSSW_13_3_0_pre4/src
