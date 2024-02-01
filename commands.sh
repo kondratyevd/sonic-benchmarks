b3c497337c2aead03323627f7c38a294f02c465c
Dmitry <kondratyev.d.95@gmail.com>
2024-02-01 21:59:14 +0100

    update CI

--- commands.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/fastmachinelearning/sonic-workflows/CMSSW_13_3_X/setup.sh
-chmod +x setup.sh
-./setup.sh
---
+++ tests/test_commands.sh
@@ -0,0 +1,11 @@
+git config --global user.email "kondratyev.d.95@gmail.com"
+git config --global user.name "Dmitry Kondratyev"
+       
+wget https://raw.githubusercontent.com/fastmachinelearning/sonic-workflows/CMSSW_13_3_X/setup.sh
+chmod +x setup.sh
+./setup.sh
+
+cd CMSSW_13_3_0_pre4/src/sonic-workflows
+cmsenv
+
+cmsRun run.py maxEvents=10000 sonic=False device="cpu"
