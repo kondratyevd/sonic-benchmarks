c5be1b4bd0aab7e10819d3a24ec08cecc97a56cf
Dmitry <kondratyev.d.95@gmail.com>
2024-02-05 23:30:44 +0100

    update perf_analyzer workflow

--- tests/test_perf_analyzer.sh
+++ tests/test_perf_analyzer.sh
@@ -3,7 +3,10 @@ cd clients
 wget -q https://github.com/triton-inference-server/server/releases/download/v2.42.0/v2.42.0_ubuntu2204.clients.tar.gz
 tar xzf v2.42.0_ubuntu2204.clients.tar.gz
 
-sudo apt-get install -y libb64-dev nvidia-cuda-toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-3 libb64-dev
 
 cd ../
 pwd
