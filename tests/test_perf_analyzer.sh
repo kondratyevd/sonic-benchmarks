53ae752871bfe581c944d8d609dbe4f5c891c0db
Dmitry <kondratyev.d.95@gmail.com>
2024-02-05 22:48:07 +0100

    update perf-analyzer workflow

--- tests/test_perf_analyzer.sh
+++ tests/test_perf_analyzer.sh
@@ -1,1 +1,10 @@
-perf_analyzer --model-repository=$PWD/models:/models --async -m deepmet -u localhost:8001 -p 1000 -b 100 --input-data "random"
+mkdir clients
+cd clients
+wget https://github.com/triton-inference-server/server/releases/download/v2.42.0/v2.42.0_ubuntu2204.clients.tar.gz
+tar xzf v2.42.0_ubuntu2204.clients.tar.gz
+
+cd ../
+pwd
+ls -l
+
+clients/bin/perf_analyzer --model-repository=models --async -m deepmet -u localhost:8001 -p 1000 -b 100 --input-data "random"
