acc7fea387bb4dc80e6b2eb3259070d62a0d48a7
Dmitry <kondratyev.d.95@gmail.com>
2024-02-05 23:39:48 +0100

    update perf_analyzer workflow

--- tests/test_perf_analyzer.sh
+++ tests/test_perf_analyzer.sh
@@ -1,15 +1,15 @@
-mkdir clients
-cd clients
-wget -q https://github.com/triton-inference-server/server/releases/download/v2.42.0/v2.42.0_ubuntu2204.clients.tar.gz
-tar xzf v2.42.0_ubuntu2204.clients.tar.gz
+# mkdir clients
+# cd clients
+# wget -q https://github.com/triton-inference-server/server/releases/download/v2.42.0/v2.42.0_ubuntu2204.clients.tar.gz
+# tar xzf v2.42.0_ubuntu2204.clients.tar.gz
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-3 libb64-dev
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+# sudo dpkg -i cuda-keyring_1.1-1_all.deb
+# sudo apt-get update
+# sudo apt-get -y install cuda-toolkit-12-3 libb64-dev
 
-cd ../
+# cd ../
 pwd
 ls -l
 
-clients/bin/perf_analyzer --model-repository=models --async -m deepmet -u localhost:8001 -p 1000 -b 100 --input-data "random"
+perf_analyzer --model-repository=models --async -m deepmet -u localhost:8001 -p 1000 -b 100 --input-data "random"
